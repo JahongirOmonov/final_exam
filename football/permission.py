97718f48e063e4d78cba74966fd477abb833a553
bekzod <toxirovbekzod0705@gmail.com>
2023-10-10 12:08:46 +0500

    added

---
+++ football/permission.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import BasePermission
+
+class AdminPermissionClass(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.roles == 3:
+            return True
+        return False 
+    
+class OwnerStadiumPermissionClass(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.roles == 2:
+            return True
+        return False 
+    
+class OwnerPermissionClass(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.id==obj.user.id:
+            return True
+        return False
--- football/views.py
+++ football/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import FieldSerializer,ReserveSerializer
+from .models import FieldModel,ReserveModel
+from django.http import JsonResponse
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .permission import OwnerPermissionClass,AdminPermissionClass,OwnerStadiumPermissionClass
 
-# Create your views here.
+
+#field
+class GetAllField(generics.ListAPIView):
+    queryset=FieldModel.objects.all()
+    serializer_class=FieldSerializer
+    permission_classes=(IsAuthenticated,)
+
+class CreateField(generics.CreateAPIView):
+    queryset=FieldModel.objects.all()
+    serializer_class=FieldSerializer
+    permission_classes=(IsAuthenticated,)
+
+class PatchField(generics.RetrieveUpdateDestroyAPIView):
+    queryset=FieldModel.objects.all()
+    serializer_class=FieldSerializer
+    permission_classes=(IsAuthenticated)
+
+#reserve
+class GetAllReserve(generics.ListAPIView):
+    queryset=ReserveModel.objects.all()
+    serializer_class=ReserveSerializer
+    permission_classes=(IsAuthenticated,)
+
+class CreateReserve(generics.CreateAPIView):
+    queryset=ReserveModel.objects.all()
+    serializer_class=ReserveSerializer
+    permission_classes=(IsAuthenticated)
+
+class PatchReserve(generics.RetrieveUpdateDestroyAPIView):
+    queryset=ReserveModel.objects.all()
+    serializer_class=ReserveSerializer
+    permission_classes=(IsAuthenticated)
