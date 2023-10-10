bfb603c40a954fa19cb23f4be12d87fd88b94e1f
mahkamovshohrux <mahkamovshohrux1909@gmail.com>
2023-10-10 14:57:22 +0500

    save

--- config/settings.py
+++ config/settings.py
@@ -106,12 +106,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
+from datetime import timedelta
+...
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5)}
 
 
 # Password validation
